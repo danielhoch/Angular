960680103bc0978156df29a33d3e9fc590819f07
Daniel Herbert Hoch <danielhoch28@gmail.com>
2020-04-13 23:25:06 -0300

    Melhorando a aplicação

--- alurapic/src/app/photos/photo-list/photo-list.resolver.ts
+++ alurapic/src/app/photos/photo-list/photo-list.resolver.ts
@@ -8,10 +8,10 @@ import { PhotoService } from './../photo/photo.service';
 @Injectable({ providedIn: 'root'})
 export class PhotoListResolver implements Resolve<Observable<Photo[]>>{
 
-  constructor(private service: PhotoService){}
+  constructor(private service: PhotoService) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Photo[]> {
     const userName = route.params.userName;
-    return this.service.listFromUser(userName);
+    return this.service.listFromUserPaginated(userName, 1);
   }
 }
--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -1,28 +1,15 @@
-import { FilterByDescription } from './photo-list/filter-by-description';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
 
-import { PhotoComponent } from './photo/photo.component';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
+import { PhotoModule } from './photo/photo.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
 
 @NgModule({
-  declarations: [
-    PhotoComponent,
-    PhotoListComponent,
-    PhotoFormComponent,
-    PhotosComponent,
-    FilterByDescription
-  ],
   imports: [
-    CommonModule,
-    HttpClientModule
+    PhotoModule,
+    PhotoFormModule,
+    PhotoListModule,
    ]
 })
 
-export class PhotosModule {
-
-}
+export class PhotosModule {}
